72a6df124a9fd07a3d6920b90eb4865c26aec5d6
Evan-otero <Evan.otero19@gmail.com>
2019-08-28 19:20:33 +0000

    Initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,26 @@ import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
+var alpha = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+var bravo = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "JOKER"
+];
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+document.querySelector(".suit").innerHTML =
+  alpha[Math.floor(Math.random() * alpha.length)];
+
+document.querySelector(".cardnumber").innerHTML =
+  bravo[Math.floor(Math.random() * alpha.length)];
