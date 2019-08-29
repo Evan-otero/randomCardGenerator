bcf3ad925d17287833d2074b041e7ab251092e18
Evan-otero <Evan.otero19@gmail.com>
2019-08-29 00:35:14 +0000

    changed heart and diamond color

--- src/js/index.js
+++ src/js/index.js
@@ -6,29 +6,22 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-var alpha = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
-var bravo = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-  "JOKER"
-];
+var alpha = ["&#9824", "&#9827", "&#9829", "&#9830"];
+var bravo = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
 let x = alpha[Math.floor(Math.random() * alpha.length)];
 
-document.querySelector(".suit").innerHTML = x;
+let delta = document.getElementById("suit");
+let echo = document.getElementById("suit1");
 
-document.querySelector(".suit1").innerHTML = x;
+delta.innerHTML = x;
+echo.innerHTML = x;
+
+if (x === "&#9829" || x === "&#9830") {
+  delta.classList.toggle("color");
+  echo.classList.toggle("color");
+}
 
 document.querySelector(".cardnumber").innerHTML =
   bravo[Math.floor(Math.random() * bravo.length)];
+console.log(x);
